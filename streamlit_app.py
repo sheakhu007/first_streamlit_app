cb94adcb4b21f1d74f59c90268989edd5b24a603
Abhishek Agrawal <63266431+sheakhu007@users.noreply.github.com>
2023-07-13 10:17:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,14 @@ Date: 13 July 2023
 Snowflake certification
 """
 import streamlit
-
+import pandas as pd
 streamlit.title("""My Parents Healthy Dinner""")
-streamlit.header("I Love You")
-streamlit.text("""I Love You 
-Because I Love YOu""")
-
-
-
-
 streamlit.header('🍞Breakfast Menu')
 streamlit.text(' 🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
-
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(df)
