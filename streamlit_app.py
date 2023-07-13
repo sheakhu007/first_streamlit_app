2ce3e57944d739da606a3fb15ac1007c4437ef3d
Abhishek Agrawal <63266431+sheakhu007@users.noreply.github.com>
2023-07-13 10:19:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,5 @@ streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(df)
+# streamlit.dataframe(df)
+streamlit.table(df)
