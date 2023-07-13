7ac8de5b58770a26a9ae9a36501415dea5e0f434
Abhishek Agrawal <63266431+sheakhu007@users.noreply.github.com>
2023-07-13 10:20:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 df = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # streamlit.dataframe(df)
-print("Table")
+streamlit.text("Table")
 streamlit.table(df)
